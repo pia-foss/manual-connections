b66fed9e75da87cc4a78f405d3776a0e6e3b5ec2
Alireza Mohamadi <sunova@tutanota.com>
2020-12-01 01:12:22 +0330

    Make script work from any location

--- openvpn_config/openvpn_down.sh
+++ openvpn_config/openvpn_down.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Remove process and route information when connection closes
-rm -rf /opt/piavpn-manual/pia_pid /opt/pia-manual/route_info
+rm -rf "$SCRIPTDIR"/pia_pid /opt/pia-manual/route_info
--- openvpn_config/openvpn_down_dnsoverwrite.sh
+++ openvpn_config/openvpn_down_dnsoverwrite.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+SCRIPTDIR=$(dirname $(realpath $BASH_SOURCE)/..)
+
 # Remove process and route information when connection closes
-rm -rf /opt/piavpn-manual/pia_pid /opt/pia-manual/route_info
+rm -rf "$SCRIPTDIR"/pia_pid /opt/pia-manual/route_info
 
 # Replace resolv.conf with original stored as backup
-cat /opt/piavpn-manual/resolv_conf_backup > /etc/resolv.conf
+cat "$SCRIPTDIR"/resolv_conf_backup > /etc/resolv.conf
