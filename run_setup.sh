08bba809dff413cfd182e1f80b052bde9cc505d2
faireOwl <72615062+faireOwl@users.noreply.github.com>
2020-12-10 16:45:20 -0700

    Formatting update
    Formatting, color, and sequence update to accentuate important information and improve flow.

--- run_setup.sh
+++ run_setup.sh
@@ -21,75 +21,79 @@
 # SOFTWARE.
 
 # Only allow script to run as
-if [ "$(whoami)" != "root" ]; then
-  echo "This script needs to be run as root. Try again with 'sudo $0'"
+#!/bin/bash
+# Copyright (C) 2020 Private Internet Access, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# This function allows you to check if the required tools have been installed.
+function check_tool() {
+  cmd=$1
+  package=$2
+  if ! command -v $cmd &>/dev/null
+  then
+    echo "$cmd could not be found"
+    echo "Please install $package"
+    exit 1
+  fi
+}
+# Now we call the function to make sure we can use curl and jq.
+check_tool curl curl
+check_tool jq jq
+
+# Define colors for output
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+mkdir -p /opt/piavpn-manual
+
+if [[ ! $PIA_USER || ! $PIA_PASS ]]; then
+  echo If you want this script to automatically get a token from the Meta
+  echo service, please add the variables PIA_USER and PIA_PASS. Example:
+  echo $ PIA_USER=p0123456 PIA_PASS=xxx ./get_token.sh
   exit 1
 fi
 
-# Erase previous authentication token if present
-rm -f /opt/piavpn-manual/token /opt/piavpn-manual/latencyList
-
-# This section asks for PIA user credentials
-echo
-echo -n "PIA username (pNNNNNNN): "
-read PIA_USER
-
-if [ -z "$PIA_USER" ]; then
-  echo Username is required, aborting.
-  exit 1
-fi
-echo
-export PIA_USER
-
-echo -n "PIA password: "
-read -s PIA_PASS
-echo
-
-if [ -z "$PIA_PASS" ]; then
-  echo Password is required, aborting.
-  exit 1
-fi
-export PIA_PASS
-
-echo -n "Checking login credentials..."
-# Confirm MAX_LATENCY allowance, then confirm credentials and generate token
-./get_token.sh
-
-# If the script failed to generate an authentication token, the script will exit early.
 tokenLocation=/opt/piavpn-manual/token
-if [ ! -f "$tokenLocation" ]; then
+  
+generateTokenResponse=$(curl -s -u "$PIA_USER:$PIA_PASS" \
+  "https://privateinternetaccess.com/gtoken/generateToken")
+
+if [ "$(echo "$generateTokenResponse" | jq -r '.status')" != "OK" ]; then
+  echo
+  echo
+  echo -e ${RED}"Could not authenticate with the login credentials provided : "
+  echo
+  echo "Username : "$PIA_USER
+  echo "Password : "$PIA_PASS
   exit 1
 fi
-
-# This section asks for connection preferences that are
-# relevant to manual server selection
-echo -n "Do you want a forwarding port assigned ([N]o/[y]es): "
-read portForwarding
+  
+echo -e ${GREEN}OK!
 echo
-
-PIA_PF="false"
-if echo ${portForwarding:0:1} | grep -iq y; then
-  PIA_PF="true"
-fi
-export PIA_PF
-echo PIA_PF=$PIA_PF
-
-# Set this to the maximum allowed latency in seconds.
-# All servers that respond slower than this will be ignored.
-echo -n "
-With no input, the maximum allowed latency will be set to 0.05s (50ms).
-If your connection has high latency, you may need to increase this value.
-For example, you can try 0.2 for 200ms allowed latency.
-Custom latency (no input required for 50ms): "
-read customLatency
+token="$(echo "$generateTokenResponse" | jq -r '.token')"
+echo -e PIA_TOKEN=$token${NC}
+echo $token > /opt/piavpn-manual/token || exit 1
+echo 
+echo This token will expire in 24 hours.
 echo
-
-MAX_LATENCY=0.05
-if [[ $customLatency != "" ]]; then
-  MAX_LATENCY=$customLatency
-fi
-export MAX_LATENCY
-echo "MAX_LATENCY=\"$MAX_LATENCY\"
 "
 
 # Prompt the user to specify a server or auto-connect to the lowest latency
