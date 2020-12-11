9dcf59831c9da328878014f12fd4af2392909c40
faireOwl <72615062+faireOwl@users.noreply.github.com>
2020-12-11 08:50:04 -0700

    Wrong content
    Update to revert to proper content

--- run_setup.sh
+++ run_setup.sh
@@ -1,49 +1,4 @@
-#!/bin/bash
-
-# Copyright (C) 2020 Private Internet Access, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# Only allow script to run as
-#!/bin/bash
-# Copyright (C) 2020 Private Internet Access, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# This function allows you to check if the required tools have been installed.
-function check_tool() {
+git@github.com:faireOwl/manual-connections.git
   cmd=$1
   package=$2
   if ! command -v $cmd &>/dev/null
@@ -112,35 +67,196 @@ if echo ${selectServer:0:1} | grep -iq y; then
   export CONNECT_TO
   PIA_AUTOCONNECT="no"
   export PIA_AUTOCONNECT
+#!/bin/bash
+
+# Copyright (C) 2020 Private Internet Access, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Only allow script to run as
+if [ "$(whoami)" != "root" ]; then
+  echo "This script needs to be run as root. Try again with 'sudo $0'"
+  exit 1
+fi
+
+# Erase previous authentication token if present
+rm -f /opt/piavpn-manual/token /opt/piavpn-manual/latencyList
+
+# Define colors for output
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# This section asks for PIA user credentials
+echo
+echo -n "PIA username (pNNNNNNN): "
+read PIA_USER
+
+if [ -z "$PIA_USER" ]; then
+  echo Username is required, aborting.
+  exit 1
+fi
+echo
+export PIA_USER
+
+echo -n "PIA password: "
+read -s PIA_PASS
+echo
+echo
+
+if [ -z "$PIA_PASS" ]; then
+  echo Password is required, aborting.
+  exit 1
+fi
+export PIA_PASS
+
+echo -n "Checking login credentials..."
+# Confirm MAX_LATENCY allowance, then confirm credentials and generate token
+./get_token.sh
+
+# If the script failed to generate an authentication token, the script will exit early.
+tokenLocation=/opt/piavpn-manual/token
+if [ ! -f "$tokenLocation" ]; then
+  exit 1
+fi
+
+# This section asks for connection preferences that are
+# relevant to manual server selection
+echo -n "Do you want a forwarding port assigned ([N]o/[y]es): "
+read portForwarding
+echo
+
+PIA_PF="false"
+if echo ${portForwarding:0:1} | grep -iq y; then
+  PIA_PF="true"
+fi
+export PIA_PF
+echo -e ${GREEN}PIA_PF=$PIA_PF${NC}
+echo
+
+# Check for the required presence of resolvconf for setting DNS on wireguard connections.
+setDNS="yes"
+if ! command -v resolvconf &>/dev/null && [ "$PIA_AUTOCONNECT" == wireguard ]; then
+  echo The resolvconf package could not be found.
+  echo This script can not set DNS for you and you will
+  echo need to invoke DNS protection some other way.
+  echo
+  setDNS="no"
+fi
+
+if [ "$setDNS" != no ]; then
+  echo Using third party DNS could allow DNS monitoring.
+  echo -n "Do you want to force PIA DNS ([Y]es/[n]o): "
+  read setDNS
+  echo
+fi
+
+PIA_DNS="true"
+if echo ${setDNS:0:1} | grep -iq n; then
+  PIA_DNS="false"
+fi
+export PIA_DNS
+echo -e ${GREEN}PIA_DNS=$PIA_DNS"
+${NC}"
+
+echo "Having active IPv6 connections might compromise security by allowing"
+echo "split tunnel connections that run outside the VPN tunnel."
+echo -n "Do you want to disable IPv6? (Y/n): "
+read disable_IPv6
+echo
+
+if echo ${disable_IPv6:0:1} | grep -iq n; then
+  echo -e ${RED}"IPv6 settings have not been altered.
+  "${NC}
+else
+  sysctl -w net.ipv6.conf.all.disable_ipv6=1
+  sysctl -w net.ipv6.conf.default.disable_ipv6=1
+  echo
+  echo -e "${GREEN}IPv6 has been disabled, you can enable it again with: "
+  echo "sysctl -w net.ipv6.conf.all.disable_ipv6=0"
+  echo "sysctl -w net.ipv6.conf.default.disable_ipv6=0"
+  echo -e ${NC}
+fi
+
+# Set this to the maximum allowed latency in seconds.
+# All servers that respond slower than this will be ignored.
+echo -n "With no input, the maximum allowed latency will be set to 0.05s (50ms).
+If your connection has high latency, you may need to increase this value.
+For example, you can try 0.2 for 200ms allowed latency.
+Custom latency (no input required for 50ms): "
+read customLatency
+echo
+
+MAX_LATENCY=0.05
+if [[ $customLatency != "" ]]; then
+  MAX_LATENCY=$customLatency
+fi
+export MAX_LATENCY
+echo -e "${GREEN}MAX_LATENCY=\"$MAX_LATENCY\"${NC}
+"
+
+# Prompt the user to specify a server or auto-connect to the lowest latency
+echo -n "Do you want to manually select a server, instead of auto-connecting to the
+server with the lowest latency ([N]o/[y]es): "
+read selectServer
+echo
+
+# Call the region script with input to create an ordered list based upon latency
+# When $CONNECT_TO is set to false, get_region.sh will generate a list of servers
+# that meet the latency requirements speciied by $MAX_LATENCY.
+# When $PIA_AUTOCONNECT is set to no, get_region.sh will sort that list of servers
+# to allow for numeric selection, or an easy manual review of options.
+if echo ${selectServer:0:1} | grep -iq y; then
+  CONNECT_TO="false"
+  export CONNECT_TO
+  PIA_AUTOCONNECT="no"
+  export PIA_AUTOCONNECT
   ./get_region.sh
   
-  # Output the ordered list of servers that meet the latency specification $MAX_LATENCY
-  i=0
-  while read line; do
-    i=$((i+1))
-    echo $i ":" $line
-  done < /opt/piavpn-manual/latencyList
+  if [ -s /opt/piavpn-manual/latencyList ]; then
+    # Output the ordered list of servers that meet the latency specification $MAX_LATENCY
+    i=0
+    while read line; do
+      i=$((i+1))
+      echo -e $i "\t: " $( awk 'NR == '$i' {print $1}' /opt/piavpn-manual/latencyList ) $( awk 'NR == '$i' {print $2}' /opt/piavpn-manual/latencyList ) '\t' $( awk 'NR == '$i' {print $3}' /opt/piavpn-manual/latencyList ) '\t' $( awk 'NR == '$i' {print $4}' /opt/piavpn-manual/latencyList ) $( awk 'NR == '$i' {print $5}' /opt/piavpn-manual/latencyList )
+    done < /opt/piavpn-manual/latencyList
   
-  # Receive input to specify the server to connect to manually
-  echo
-  echo -n "Input the number of the server you want to connect to ([1]-[$i]) : "
-  read serverSelection
-  echo
+    # Receive input to specify the server to connect to manually
+    echo
+    echo -n "Input the number of the server you want to connect to ([1]-[$i]) : "
+    read serverSelection
 
-  if [[ -z "$serverSelection" ]]; then
-    echo Invalid input, you must input the number of the server you want to connect to.
-    exit 1
-  else
-    bestServer=$( awk 'NR == '$serverSelection' {print $4 $5}' /opt/piavpn-manual/latencyList )
-    CONNECT_TO=$( awk 'NR == '$serverSelection' {print $2}' /opt/piavpn-manual/latencyList )
-  fi
+    if [[ -z "$serverSelection" ]]; then
+      echo -e ${RED}Invalid input, you must input the number of the server you want to connect to.
+      exit 1
+    else
+      CONNECT_TO=$( awk 'NR == '$serverSelection' {print $2}' /opt/piavpn-manual/latencyList )
+    fi
   
-  # Write the serverID for use when connecting, and display the serverName for user confirmation
-  export CONNECT_TO
-  echo You will attempt to connect to $bestServer.
-  echo
+    # Write the serverID for use when connecting, and display the serverName for user confirmation
+    export CONNECT_TO
+    echo
+  else
+    exit 1
+  fi
 else
-  echo You will auto-connect to the server with the lowest latency.
+  echo -e ${GREEN}You will auto-connect to the server with the lowest latency.${NC}
   echo
 fi
 
@@ -173,17 +289,10 @@ if echo ${connection_method:0:1} | grep -iq o; then
   PIA_AUTOCONNECT="openvpn_${protocol}_${encryption}"
 fi
 export PIA_AUTOCONNECT
-echo PIA_AUTOCONNECT=$PIA_AUTOCONNECT"
-"
+echo -e ${GREEN}PIA_AUTOCONNECT=$PIA_AUTOCONNECT"
+${NC}"
 
-# Check for the required presence of resolvconf for setting DNS on wireguard connections.
-setDNS="yes"
-if ! command -v resolvconf &>/dev/null && [ "$PIA_AUTOCONNECT" == wireguard ]; then
-  echo The resolvconf package could not be found.
-  echo This script can not set DNS for you and you will
-  echo need to invoke DNS protection some other way.
-  echo
-  setDNS="no"
+./get_region.sh
 fi
 
 if [ "$setDNS" != no ]; then
