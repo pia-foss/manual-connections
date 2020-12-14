2ad18501ea05ce98576ed4f985a74163a6d73308
faireOwl <72615062+faireOwl@users.noreply.github.com>
2020-12-14 08:19:26 -0700

    Update to allow for 100+ servers
    Updated output formatting to allow for a server list that exceeds 100 items.

--- run_setup.sh
+++ run_setup.sh
@@ -174,7 +174,7 @@ if echo ${selectServer:0:1} | grep -iq y; then
       location3=$( awk 'NR == '$i' {print $6}' /opt/piavpn-manual/latencyList )
       location4=$( awk 'NR == '$i' {print $7}' /opt/piavpn-manual/latencyList )
       location=$location1" "$location2" "$location3" "$location4
-      printf " %2s : %-8s %-15s %17s" $i $time $ip $id
+      printf " %3s : %-8s %-15s %17s" $i $time $ip $id
       echo " - "$location
     done < /opt/piavpn-manual/latencyList
   
